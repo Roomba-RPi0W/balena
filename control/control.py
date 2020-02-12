f04c912f81ed5f11dd03502eb8a179c648e67c4c
Kimmo Huoman <kipenroskaposti@gmail.com>
2020-02-13 00:59:58 +0200

    In theory, everything should be setup and ready. Double-check with serial monitor before actually plugging in TODO.

--- control/control.py
+++ control/control.py
@@ -1,20 +1,34 @@
 import zmq
+from pyroombaadapter import PyRoombaAdapter
 
-context = zmq.Context()
-socket = context.socket(zmq.REP)
-socket.bind("tcp://*:5555")
 
-while True:
-    #  Wait for next request from client
-    message = socket.recv().decode('utf-8')
+class RoombaControl(PyRoombaAdapter):
+    def __init__(self):
+        super().__init__(port='/dev/serial0')
 
-    if message == 'clean':
-        print('Cleaning, as requested!')
-        socket.send(b"ok")
-    elif message == 'stop':
-        print('Halting to a stop, as requested!')
-        socket.send(b"ok")
-    else:
-        print('WTF are you going on about?')
-        #  Send reply back to client
-        socket.send(b"fail")
+    def wake_up(self):
+        # TODO: Raise BRC to HIGH to wake up the Roomba
+        pass
+
+
+if __name__ == '__main__':
+    context = zmq.Context()
+    socket = context.socket(zmq.REP)
+    socket.bind("tcp://*:5555")
+
+    while True:
+        #  Wait for next request from client
+        message = socket.recv().decode('utf-8')
+
+        if message == 'clean':
+            print('Cleaning, as requested!')
+            RoombaControl().start_cleaning()
+            socket.send(b"ok")
+        elif message == 'stop':
+            print('Halting to a stop, as requested!')
+            RoombaControl().turn_off_power()
+            socket.send(b"ok")
+        else:
+            print('WTF are you going on about?')
+            #  Send reply back to client
+            socket.send(b"fail")
