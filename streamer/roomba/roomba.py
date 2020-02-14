55fcb87bdbaeb6b00a6fced03e6a10bb0e13c000
Kimmo Huoman <kipenroskaposti@gmail.com>
2020-02-14 18:57:21 +0200

    Cleanup.

--- streamer/roomba/roomba.py
+++ streamer/roomba/roomba.py
@@ -12,53 +12,37 @@ class RoombaStreamingHandler(StreamingHandler):
             os.path.join(os.path.dirname(os.path.realpath(__file__)), 'templates')
         ] + super().template_paths
 
+    def __send_to_zmq(self, command):
+        context = zmq.Context()
+        socket = context.socket(zmq.REQ)
+        socket.connect('tcp://control:5555')
+        socket.send(command)
+        return socket.recv().decode('utf-8')
+
+    def __handle_command(self, command):
+        reply = self.__send_to_zmq(command)
+        content = '{"status": "%s"}' % reply
+        self.send_response(200 if reply == 'ok' else 500)
+        self.send_header('Content-Type', 'application/json')
+        self.send_header('Content-Length', len(content))
+        self.end_headers()
+        self.wfile.write(content.encode('utf-8'))
+
     def do_POST(self):
         if self.path == '/api/clean':
-            context = zmq.Context()
-            socket = context.socket(zmq.REQ)
-            socket.connect('tcp://control:5555')
-            socket.send(b'clean')
-            reply = socket.recv().decode('utf-8')
-
-            content = '{"status": "%s"}' % reply
-            self.send_response(200 if reply == 'ok' else 500)
-            self.send_header('Content-Type', 'application/json')
-            self.send_header('Content-Length', len(content))
-            self.end_headers()
-            self.wfile.write(content.encode('utf-8'))
+            self.__handle_command(b'clean')
             return
 
         if self.path == '/api/dock':
-            context = zmq.Context()
-            socket = context.socket(zmq.REQ)
-            socket.connect('tcp://control:5555')
-            socket.send(b'dock')
-            reply = socket.recv().decode('utf-8')
-
-            content = '{"status": "%s"}' % reply
-            self.send_response(200 if reply == 'ok' else 500)
-            self.send_header('Content-Type', 'application/json')
-            self.send_header('Content-Length', len(content))
-            self.end_headers()
-            self.wfile.write(content.encode('utf-8'))
+            self.__handle_command(b'dock')
             return
 
         if self.path == '/api/stop':
-            context = zmq.Context()
-            socket = context.socket(zmq.REQ)
-            socket.connect('tcp://control:5555')
-            socket.send(b'stop')
-            reply = socket.recv().decode('utf-8')
-
-            content = '{"status": "%s"}' % reply
-            self.send_response(200 if reply == 'ok' else 500)
-            self.send_header('Content-Type', 'application/json')
-            self.send_header('Content-Length', len(content))
-            self.end_headers()
-            self.wfile.write(content.encode('utf-8'))
+            self.__handle_command(b'stop')
             return
 
         return super().do_POST()
+
 
 if __name__ == '__main__':
     try:
